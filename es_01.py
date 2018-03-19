131c01100e838edabc1a1ad24fcf77d90252e480
sordinho <s228457@studenti.polito.it>
2018-03-19 15:10:14 +0100

    Esercizio 01

---
+++ es_01.py
@@ -0,0 +1,10 @@
+# Esercizio numero 1 input e somma di due numeri
+
+print("Inserisci il primo numero: ")
+x = int(input())
+print("Inserisci il primo numero: ")
+y = int(input())
+
+somma = x + y
+
+print("La somma vale: " + str(somma))

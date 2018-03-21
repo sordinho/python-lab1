ad5754a250cca5052c487116e66f1f8e5e70b6e8
Davide Sordi <davi.sordi96@gmail.com>
2018-03-21 22:40:39 +0100

    Esercizio 3 funzionante

--- es_03.py
+++ es_03.py
@@ -1,24 +1,22 @@
 # To Do manager
 
-def printMenu():
+def printMenu(): #Funzione per la stampa del menu
     print("------------------------------------------")
     print("1) Insert a new task")
     print("2) Remove a task")
     print("3) Show tasks")
     print("4) Exit")
-    print("Inserire la scelta ---> ")
 
-printMenu()
-cmd = input()
-n = 0
 tasks = list()
+
 while cmd != '4':
     printMenu()
+    cmd = input("Inserire il comando: ")
     if cmd == '1':
-        task = input()
-        tasks.append[task]
+        task = input("Inserisci un task: ")
+        tasks.append(task)
     elif cmd == '2':
-        target = input()
+        target = input("Inserisci un task da rimuovere: ")
         tasks.remove(target)
     elif cmd == '3':
         print(tasks)
